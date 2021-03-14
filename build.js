ad1f4db3da648057f2c63dd636b987c65cc13ff9
luncheon <takeuck+github@gmail.com>
2021-03-14 11:55:42 +0900

    fix: ripples on its parent, not the document.body. add esm (.mjs) bundle.

--- build.js
+++ build.js
@@ -14,4 +14,25 @@ if (process.argv.includes('--serve')) {
   esbuild.serve({ servedir: '.' }, options).then(console.log('http://localhost:8000'))
 } else {
   esbuild.build(options)
+  esbuild.build({
+    ...options,
+    entryPoints: ['material/material.ts'],
+    format: 'esm',
+    outdir: undefined,
+    outfile: 'material/material.mjs',
+    plugins: [
+      {
+        name: 'local',
+        setup: build => {
+          // make node_modules external
+          build.onResolve({ filter: /^[^.]/ }, args => ({ path: args.path, external: true }))
+
+          // ignore css/scss
+          build.onResolve({ filter: /\.s?css$/ }, args => ({ path: args.path, namespace: 'ignore' }))
+          build.onLoad({ filter: /.*/, namespace: 'ignore' }, () => ({ contents: '' }))
+        },
+      },
+      ...options.plugins,
+    ],
+  })
 }
--- material/material.ts
+++ material/material.ts
@@ -1,17 +1,18 @@
+import { mdiCheckboxBlankOffOutline, mdiCheckboxBlankOutline, mdiCheckboxMarked, mdiMinusBox } from '@mdi/js'
 import ripplet from 'ripplet.js/es/ripplet-declarative'
 import { elab } from '../core/core'
 import './material.scss'
 
 elab({
   popupTemplate: '<ul><slot></slot></ul>',
-  popupItemTemplate: `<li><label class=elab-label data-ripplet="color: var(--elab-color);">
+  popupItemTemplate: `<li><label class=elab-label data-ripplet="append-to:parent;color:var(--elab-color);">
 <div class=elab-label-background></div>
 <input class=elab-checkbox type=checkbox>
-<svg class=elab-checkbox-icon viewBox="0 0 24 24" fill=currentColor data-ripplet="opacity: .2; centered: true;">
-  <path class=elab-checkbox-icon-unchecked d="M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z" />
-  <path class=elab-checkbox-icon-disabled d="M22.11 21.46L2.39 1.73L1.11 3L3 4.9V19C3 20.11 3.9 21 5 21H19.1L20.84 22.73L22.11 21.46M5 19V6.89L17.11 19H5M8.2 5L6.2 3H19C20.1 3 21 3.89 21 5V17.8L19 15.8V5H8.2Z" />
-  <path class=elab-checkbox-icon-checked d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
-  <path class=elab-checkbox-icon-indeterminate d="M17,13H7V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
+<svg class=elab-checkbox-icon viewBox="0 0 24 24" data-ripplet="append-to:parent;opacity:.2;centered:true;">
+  <path class=elab-checkbox-icon-unchecked d="${mdiCheckboxBlankOutline}" />
+  <path class=elab-checkbox-icon-disabled d="${mdiCheckboxBlankOffOutline}" />
+  <path class=elab-checkbox-icon-checked d="${mdiCheckboxMarked}" />
+  <path class=elab-checkbox-icon-indeterminate d="${mdiMinusBox}" />
 </svg>
 <div class=elab-content><slot></slot></div>
 </label></li>`,
@@ -26,5 +27,9 @@ addEventListener('pointerout', clearRipplet)
 addEventListener('pointercancel', clearRipplet)
 addEventListener('pointerdown', event => {
   const elab = (event.target as Element).closest('.elab')
-  elab && ripplet({ currentTarget: elab, clientX: event.clientX, clientY: event.clientY }, { clearing: false, color: 'var(--elab-color)' })
+  elab &&
+    ripplet(
+      { currentTarget: elab, clientX: event.clientX, clientY: event.clientY },
+      { appendTo: 'parent', clearing: false, color: 'var(--elab-color)' },
+    )
 })
